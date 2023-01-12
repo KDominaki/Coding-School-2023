28b75b6ecd5244a74a748672ce72a6b568a27443
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-12 13:18:20 +0200

    S4 - Q4 added Q2 modified

--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -5,11 +5,21 @@ string name = "Kostas";
 Console.WriteLine("Hello " + name);
 
 // 2. Write a C# program to print the sum of two numbers and the devision of those numbers
-short num1 = 12;
-short num2 = 6;
+int num1 = 12;
+int num2 = 6;
 int sum = num1 + num2;
 float division = num1 / num2;
 
 Console.WriteLine(num1 + "+" + num2 + "=" + sum + " and " + num1 + "/" + num2 + "=" + division);
+
+// 3.>> the result of the specified operstions
+
+/* 4.Write a C# program that assigns an age (number) (for example 20) and 
+ * a gender (string) (for example female) and displays something like: "You are female and look younger than 20."*/
+
+string gender = "female";
+short age = 20;
+
+Console.WriteLine("You are "+gender+" and you are "+age);
 
 Console.ReadLine();
