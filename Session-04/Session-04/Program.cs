4c44bbf289e5808e1ff1298dae11280a3a46f56a
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-12 12:47:37 +0200

    Session-04 and q1 added

---
+++ Session-04/Session-04/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// 1. Write a C# program to print Hello and your name in the same line
+string name = "Kostas";
+Console.WriteLine("Hello " + name);
+
