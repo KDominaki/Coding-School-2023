a9f8d0ca4e94ecd8a1d7b2d9297d306cb274d4ce
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-12 12:58:32 +0200

    q2 added

--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -4,3 +4,12 @@
 string name = "Kostas";
 Console.WriteLine("Hello " + name);
 
+// 2. Write a C# program to print the sum of two numbers and the devision of those numbers
+short num1 = 12;
+short num2 = 6;
+int sum = num1 + num2;
+float division = num1 / num2;
+
+Console.WriteLine(num1 + "+" + num2 + "=" + sum + " and " + num1 + "/" + num2 + "=" + division);
+
+Console.ReadLine();
