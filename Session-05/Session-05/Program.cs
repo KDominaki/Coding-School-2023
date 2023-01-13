6b3498a46e9260f04396d7fa3a0cf337167450ee
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-13 19:28:41 +0200

    S5-Q2 added

--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -1,2 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Session_05;
+// Question 1
+
+// Question 2
+UsersNum numForCalc = new UsersNum();
+
+Console.WriteLine("Please enter an intiger:");
+int usersNumber = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Would you like to sum all the numbers up to {usersNumber}, or to multiply them? (enter 'S' to sum or 'M' to multiply");
+string calculation = Console.ReadLine();
+
+if ( calculation == "S")
+{
+    Console.WriteLine(numForCalc.SumAll(usersNumber));
+}
+else if ( calculation == "M")
+{
+    Console.WriteLine(numForCalc.ProductAll(usersNumber));
+}
+else
+{
+    Console.WriteLine("I am sorry but this wasn't an acceptable asnwer");
+}
+
+
+// Question 3
+
+// Question 4
+
+// Question 5
+
+Console.ReadLine();
---
+++ Session-05/Session-05/UsersNum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_05
+{
+    internal class UsersNum
+    {
+        public int SumAll(int num) 
+        {
+            int res = 0;
+
+            for (int i = 0; i < num + 1; i++)
+            {
+                res += i;
+            }
+
+            return res;
+        }
+
+        public int ProductAll(int num)
+        {
+            int res = 1;
+
+            for (int i = 1; i < num + 1; i++)
+            {
+                res *= i;
+            }
+
+            return res;
+        }
+    }
+}
