25f7eaf4b0c07cabc3530dcc0c77871c3f599696
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-14 12:23:17 +0200

    S5-Q3 added

--- Session-05/Session-05/Program.cs
+++ Session-05/Session-05/Program.cs
@@ -27,6 +27,33 @@ else
 
 // Question 3
 
+Console.WriteLine("Give me a number and i will give you all the prime numbers up to this number:");
+usersNumber= Convert.ToInt32(Console.ReadLine());
+
+int[] primes = new int[usersNumber];
+
+for (int i = 1; i < usersNumber; i++)
+{
+    if (i == 2)
+    {
+        primes[i-1] = i;
+    }
+    else if (i % 2 != 0) 
+    {
+        primes[i-1] = i;
+    }
+    else {}
+}
+
+foreach (int num in primes)
+{
+    if(num == 0) { }
+    else { Console.WriteLine(num); }
+    
+}
+
+
+
 // Question 4
 
 // Question 5
