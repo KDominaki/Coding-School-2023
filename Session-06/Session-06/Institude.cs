ba276d04ccbd15865c59c2296547401d9efe5a31
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-16 15:03:32 +0200

    S6- Institude done

--- Session-06/Session-06/Institude.cs
+++ Session-06/Session-06/Institude.cs
@@ -8,5 +8,34 @@ namespace Session_06
 {
     internal class Institude
     {
+        public Guid ID { get; set; }
+        public string Name { get; set; }
+        public int YearsInService { get; set; }
+
+        public Institude()
+        {
+            
+        }
+
+        public Institude(Guid id)
+        {
+            ID = id;
+        }
+
+        public Institude(Guid id, string name, int yearsInService)
+        {
+            Name = name;
+            YearsInService = yearsInService;
+            ID = id;
+        }
+
+        public Institude(Guid id, string name)
+        {
+            Name= name;
+            ID = id;
+        }
+
+        public void GetName() { }
+        public void SetName(string name) { }
     }
 }
