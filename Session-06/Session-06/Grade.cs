e1a58347c02bc14e829387c1f823114dce31bf63
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-16 15:24:05 +0200

    S6-Grade done

--- Session-06/Session-06/Grade.cs
+++ Session-06/Session-06/Grade.cs
@@ -8,7 +8,21 @@ namespace Session_06
 {
     internal class Grade
     {
-        public Guid Id { get; set; }
-        
+        public Guid ID { get; set; }
+        public Guid StudentID { get; set; }
+        public Guid CourseID { get; set; }
+        public int GradeNum { get; set; } /* Επειδή το 'Grade' που αναφέται στην άσκηση 'χτυπάει' του έδωσα αυτό το όνομα για
+                                           * ξεχωρίζει απο το όνομα της κλάσης */
+        public Grade()
+        {
+
+        }
+        public Grade(Guid id, Guid studentId, Guid courseId, int gradeNum)
+        {
+            ID= id;
+            StudentID= studentId;
+            CourseID= courseId;
+            GradeNum= gradeNum;
+        }
     }
 }
