faba09d28263e2f1101ed0aeea4037ab8360d2d3
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-19 15:00:02 +0200

    S9- Basic Fuctionallity for 1, 2, = and +

--- Session-09/Session-09/Form1.cs
+++ Session-09/Session-09/Form1.cs
@@ -2,6 +2,20 @@ namespace Session_09
 {
     public partial class Form1 : Form
     {
+        private decimal? _value1 = null;
+        private decimal? _value2 = null;
+        private decimal? _result = null;
+
+        private CalcOperation _calcOperation;
+
+        enum CalcOperation
+        {
+            Addition,
+            Division,
+            Multiplication,
+            Substraction
+        }
+
         public Form1()
         {
             InitializeComponent();
@@ -9,12 +23,54 @@ namespace Session_09
 
         private void button1_Click(object sender, EventArgs e)
         {
+            //textBox.Text += "1";
+
+            if (_result != null)
+            {
+
+                textBox.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result = null;
+            }
+
             textBox.Text += "1";
+
+            if (_value1 == null)
+            {
+                _value1 = 1;
+            }
+            else
+            {
+                _value2 = 1;
+            }
+
         }
+
+
 
         private void button2_Click(object sender, EventArgs e)
         {
+            //textBox.Text += "2";
+            if (_result != null)
+            {
+
+                textBox.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result = null;
+            }
+
             textBox.Text += "2";
+
+            if (_value1 == null)
+            {
+                _value1 = 2;
+            }
+            else
+            {
+                _value2 = 2;
+            }
         }
 
         private void button3_Click(object sender, EventArgs e)
@@ -60,6 +116,7 @@ namespace Session_09
         private void button11_Click(object sender, EventArgs e)
         {
             textBox.Text += "+";
+            _calcOperation = CalcOperation.Addition;
         }
 
         private void button12_Click(object sender, EventArgs e)
@@ -90,6 +147,18 @@ namespace Session_09
         private void button17_Click(object sender, EventArgs e)
         {
             textBox.Text += "=";
+            switch (_calcOperation)
+            {
+                case CalcOperation.Addition:
+                    _result = _value1 + _value2;
+                    break;
+
+                default:
+                    break;
+            }
+            textBox.Text += _result;
         }
+
+    
     }
 }
