84b7f314118eef1ba9acbae404db95e1f55db0e3
gregavg <gregavg@gmail.com>
2023-01-23 18:07:53 +0200

    Petshop class created

---
+++ Session-11/ClassLibrary1/PetShop.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1 {
+    public class PetShop {
+        //PROPERTIES
+        public string Name { get; set; }
+        public List<Transaction>? Transactions = new List<Transaction>();
+        public List<PetFood>? Foods = new List<PetFood>();
+        public List<Pet> Pets = new List<Pet>();
+        public List<Employee> Employees = new List<Employee>();
+        public List<Customer> Customer = new List<Customer>();
+        
+        //CTOR
+        public PetShop() {
+            Name = "The Pet";
+            Transactions.Add(new Transaction() {
+                
+            });
+        }
+
+        
+
+    }
+}
