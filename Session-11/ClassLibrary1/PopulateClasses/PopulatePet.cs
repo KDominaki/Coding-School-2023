c38208f292f433a10f16ea3fbc9cace36a44d002
gregavg <gregavg@gmail.com>
2023-01-23 19:51:16 +0200

    Populate pets commited

--- Session-11/ClassLibrary1/PopulateClasses/PopulatePet.cs
+++ Session-11/ClassLibrary1/PopulateClasses/PopulatePet.cs
@@ -19,7 +19,7 @@ namespace PopulateClassLibrary {
             Pet pet = new Pet() {
                 Animaltype = AnimalType.Dog,
                 Petstatus = PetStatus.OK,
-                ID = Guid.NewGuid(),
+                ID = Guid.Parse("{BC9B4A5D-3663-46B4-8578-E94EE1E5D452}"),
                 Breed = "Labrador",
                 Price = 1500,
                 Cost = 1000,
@@ -30,7 +30,7 @@ namespace PopulateClassLibrary {
             pet = new Pet() {
                 Animaltype = AnimalType.Cat,
                 Petstatus = PetStatus.OK,
-                ID = Guid.NewGuid(),
+                ID = Guid.Parse("{055A7F84-1EA5-430D-AC88-72EE519EB5DE}"),
                 Breed = "Siamese",
                 Price = 1300,
                 Cost = 900,
@@ -39,9 +39,31 @@ namespace PopulateClassLibrary {
             pets.Add(pet);
 
             pet = new Pet() {
-                Animaltype = AnimalType.Cat,
+                Animaltype = AnimalType.Dog,
                 Petstatus = PetStatus.OK,
-                ID = Guid.NewGuid(),
+                ID = Guid.Parse("{A639ABBD-B2E0-45EA-967A-38681FDF22ED}"),
+                Breed = "West Highland",
+                Price = 1000,
+                Cost = 700,
+
+            };
+            pets.Add(pet);
+
+            pet = new Pet() {
+                Animaltype = AnimalType.Parrot,
+                Petstatus = PetStatus.OK,
+                ID = Guid.Parse("{57AF4991-7A8B-48CF-8BAF-3C9E7AA69855}"),
+                Breed = "Parakeet",
+                Price = 100,
+                Cost = 20,
+
+            };
+            pets.Add(pet);
+
+            pet = new Pet() {
+                Animaltype = AnimalType.Cat,
+                Petstatus = PetStatus.Recovering,
+                ID = Guid.Parse("{F44DA0A9-DE5D-4D49-AFFF-83E49B2B7CE0}"),
                 Breed = "Siamese",
                 Price = 1300,
                 Cost = 900,
@@ -51,8 +73,8 @@ namespace PopulateClassLibrary {
             pet = new Pet() {
                 Animaltype = AnimalType.Cat,
                 Petstatus = PetStatus.Unhealthy,
-                ID = Guid.NewGuid(),
-                Breed = "Siamese",
+                ID = Guid.Parse("{E563D602-2C83-4457-AD7E-9D7D90BCB974}"),
+                Breed = "Sphynx",
                 Price = 1300,
                 Cost = 900,
 
@@ -62,7 +84,7 @@ namespace PopulateClassLibrary {
             pet = new Pet() {
                 Animaltype = AnimalType.Cat,
                 Petstatus = PetStatus.OK,
-                ID = Guid.NewGuid(),
+                ID = Guid.Parse("{B5F45E06-3AE4-4E17-9DBF-9A9F46B0B8B1}"),
                 Breed = "Himalayan",
                 Price = 1400,
                 Cost = 1000,
@@ -73,7 +95,7 @@ namespace PopulateClassLibrary {
             pet = new Pet() {
                 Animaltype = AnimalType.Dog,
                 Petstatus = PetStatus.Recovering,
-                ID = Guid.NewGuid(),
+                ID = Guid.Parse("{D833D529-8C94-451E-B4B2-83E55A9FAA6D}"),
                 Breed = "Malinois",
                 Price = 1300,
                 Cost = 800,
@@ -82,12 +104,45 @@ namespace PopulateClassLibrary {
             pets.Add(pet); 
             
             pet = new Pet() {
-                Animaltype = AnimalType.Dog,
-                Petstatus = PetStatus.Recovering,
-                ID = Guid.NewGuid(),
-                Breed = "Malinois",
-                Price = 1300,
-                Cost = 800,
+                Animaltype = AnimalType.Parrot,
+                Petstatus = PetStatus.OK,
+                ID = Guid.Parse("{3ED3C244-D9E2-4D10-A963-7098FCEDEA28}"),
+                Breed = "Macaw",
+                Price = 2000,
+                Cost = 1200,
+
+            };
+            pets.Add(pet);
+
+            pet = new Pet() {
+                Animaltype = AnimalType.Parrot,
+                Petstatus = PetStatus.OK,
+                ID = Guid.Parse("{65689F6F-F4A9-4297-99A0-252702C6C7A2}"),
+                Breed = "Macaw",
+                Price = 2000,
+                Cost = 1200,
+
+            };
+            pets.Add(pet);
+
+            pet = new Pet() {
+                Animaltype = AnimalType.Parrot,
+                Petstatus = PetStatus.OK,
+                ID = Guid.Parse("{6A3C6516-3DFA-4341-A984-72B0C2EBE5FF}"),
+                Breed = "African Grey",
+                Price = 1400,
+                Cost = 900,
+
+            };
+            pets.Add(pet);
+
+            pet = new Pet() {
+                Animaltype = AnimalType.Parrot,
+                Petstatus = PetStatus.OK,
+                ID = Guid.Parse("{84AE443B-869C-41FB-B5D8-2E38E2A77AA2}"),
+                Breed = "Cockatiel",
+                Price = 200,
+                Cost = 80,
 
             };
             pets.Add(pet);
