7ed15fb218128c03598d9cee06ebf43652b7905f
gregavg <gregavg@gmail.com>
2023-01-23 21:08:38 +0200

    LOGO

--- Session-11/Session-11/Form1.Designer.cs
+++ Session-11/Session-11/Form1.Designer.cs
@@ -28,24 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.btnManage = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // btnManage
             // 
-            this.button1.Location = new System.Drawing.Point(713, 415);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnManage.Location = new System.Drawing.Point(425, 134);
+            this.btnManage.Name = "btnManage";
+            this.btnManage.Size = new System.Drawing.Size(229, 149);
+            this.btnManage.TabIndex = 0;
+            this.btnManage.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Session_11.Properties.Resources.shoplogo2;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(115, 134);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(229, 149);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnManage);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -55,6 +67,7 @@
 
         #endregion
 
-        private Button button1;
+        private Button btnManage;
+        private Button button2;
     }
 }
