4f3240a6b487cbe3448914f16fdbc2aad6ee2db4
gregavg <gregavg@gmail.com>
2023-01-23 19:14:02 +0200

    deleted populate library -> created new folder

---
+++ Session-11/ClassLibrary1/PopulateClasses/PopulateCoustumers.cs
@@ -0,0 +1,15 @@
+﻿namespace PopulateClassLibrary
+{
+    public class PopulateCoustumers
+    {
+        public PopulateCoustumers() 
+        {
+            object[] array = new object[]
+            {
+
+            };
+ 
+        }
+
+    }
+}
---
+++ Session-11/ClassLibrary1/PopulateClasses/PopulateTransactions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace PopulateClassLibrary {
+    public class PopulateTransactions {
+
+
+
+
+        public PopulateTransactions() { }
+
+
+        //public List<Transaction> GetPopulateTransactions() {
+
+        //    List<Transaction> transactions = new List<Transaction>();
+        //    Transaction tr;
+
+        //    tr = new Transaction() {
+        //        ID = Guid.NewGuid(),
+
+        //    };
+        //    //Transaction tr = new Transaction();
+
+        //}
+
+    }
+}
+
+
+
+
+
+
+
+
