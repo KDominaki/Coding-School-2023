421600425c9ed7e7c524f953766ea141511951df
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-24 14:06:08 +0200

    S11- IncomeCalc method added

--- Session-11/ClassLibrary1/MonthlyLedger.cs
+++ Session-11/ClassLibrary1/MonthlyLedger.cs
@@ -10,15 +10,23 @@ namespace ClassLibrary1
     {
         public DateTime Year { get; set; }
         public DateTime Month { get; set; }
-        public decimal Incomee { get; set; }
-        public decimal Expense { get; set; } 
+        public decimal? Incomee { get; set; }
+        public decimal? Expense { get; set; } 
 
-        public decimal Total { get; set; }
+        public decimal? Total { get; set; }
 
         public MonthlyLedger()
         {
             Expense += 2000;
         }
 
+        public void incomeCalc()
+        {
+            PetShop shop= new PetShop();
+            foreach (var item in shop.Transactions)
+            {
+                Expense += item.TotalPrice;
+            }
+        }
     }
 }
