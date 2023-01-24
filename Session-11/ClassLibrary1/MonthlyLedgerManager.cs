b315cc685d653a128980bfed05d912113f73f4f0
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-24 14:35:53 +0200

    S11- MonthlyLedgerManager added

---
+++ Session-11/ClassLibrary1/MonthlyLedgerManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class MonthlyLedgerManager
+    {
+        private DateTime _date = DateTime.Now;
+        public List<MonthlyLedger> mothLegList = new List<MonthlyLedger> ();
+        
+        public void  MonthlyLedgerGenerator()
+        {
+            //MonthlyLedger monthlyLedger = new MonthlyLedger(_date.Year, _date.Month );
+            mothLegList.Add(new MonthlyLedger(_date.Year, _date.Month));
+        }
+    }
+}
