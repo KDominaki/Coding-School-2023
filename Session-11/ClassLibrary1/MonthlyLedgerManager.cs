709c14da24e5958d3da9db4df8b1312897597c81
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-24 14:52:13 +0200

    S11 - Current month changer added

--- Session-11/ClassLibrary1/MonthlyLedgerManager.cs
+++ Session-11/ClassLibrary1/MonthlyLedgerManager.cs
@@ -10,11 +10,17 @@ namespace ClassLibrary1
     {
         private DateTime _date = DateTime.Now;
         public List<MonthlyLedger> mothLegList = new List<MonthlyLedger> ();
+        public MonthlyLedger currentMonth { get; set; }
         
         public void  MonthlyLedgerGenerator()
         {
             //MonthlyLedger monthlyLedger = new MonthlyLedger(_date.Year, _date.Month );
             mothLegList.Add(new MonthlyLedger(_date.Year, _date.Month));
+        }
+
+        public void CurrentMonthChanger()
+        {
+            currentMonth = mothLegList[^1];
         }
 
         public void GeneratorActivator()
