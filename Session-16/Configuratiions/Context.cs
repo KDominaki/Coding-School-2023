fb56906c7a20d466db2a997aac213f512f3f427f
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-01-30 20:17:24 +0200

    S16 - Context and Configuration Classes added

---
+++ Session-16/Configuratiions/Context.cs
@@ -0,0 +1,35 @@
+﻿using ConfigurationsLibrary;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Context:DbContext 
+    {
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<PetFood> Petfoods { get;set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new PetConfiguration());
+            modelBuilder.ApplyConfiguration(new PetFoodConfiguration());
+            modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+            modelBuilder.ApplyConfiguration(new EmployeeConfiguration());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
--- Session-16/Configuratiions/PetConfiguration.cs
+++ Session-16/Configuratiions/PetConfiguration.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ClassLibrary1;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,13 @@ using System.Threading.Tasks;
 
 namespace ConfigurationsLibrary
 {
-    public class PetConfiguration
+    public class PetConfiguration : IEntityTypeConfiguration<Pet>
     {
+        public void Configure(EntityTypeBuilder<Pet> builder)
+        {
+            builder.ToTable("Pets");
+            builder.HasKey(pet => pet.ID);
+            builder.Property(pet => pet.ID).ValueGeneratedOnAdd();
+        }
     }
 }
