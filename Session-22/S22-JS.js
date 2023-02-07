ff31837a764680fdf1e12340e4d36260d6a6ae36
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-02-07 13:30:32 +0200

    S22 - Ex 1 function done

--- Session-22/S22-JS.js
+++ Session-22/S22-JS.js
@@ -1,1 +1,15 @@
 // JavaScript source code
+
+//Ex 1
+function reverse(s) {
+    return s.split("").reverse().join("");
+}
+
+//Ex 2
+
+
+//Ex 4
+function multiply(a, b) {
+    return a * b;
+}
+// Ex 5
