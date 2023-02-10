73f68185adf409a6591a664c3c4b05f1021e751a
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-02-10 16:07:09 +0200

    S23 - MonthlyLedger added

---
+++ Session-23/PetShop.Model/MonthlyLedger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetShop.Model
+{
+    public class MonthlyLedger
+    {
+        public decimal Expenses { get; set; }
+        public decimal Income { get; set; }
+        public decimal Total { get; set; }
+        public DateTime Date { get; set; }
+        public IList<Transaction> Transactions { get; set; }
+        public IList<Employee> Employees { get; set; }
+
+        public MonthlyLedger(IList<Transaction> transactions, IList<Employee> employees) 
+        {
+            Transactions = transactions;
+            Employees = employees;
+            Expenses= 2000;
+            Date= DateTime.Now;
+        }
+
+        public void TotalCalc()
+        {
+            foreach(var transaction in Transactions)
+            {
+                Income += transaction.TotalPrice;
+            }
+            foreach(var employee in Employees)
+            {
+                Expenses += employee.SalaryPerMonth;
+            }
+
+            Total = Income - Expenses;
+        }
+    }
+}
