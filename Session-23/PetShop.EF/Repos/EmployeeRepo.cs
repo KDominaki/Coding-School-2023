0a86731e2569ae87a61fc5c236564973016a0450
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-02-11 10:21:55 +0200

    S23 - A critical Error has been fixed

--- Session-23/PetShop.EF/Repos/EmployeeRepo.cs
+++ Session-23/PetShop.EF/Repos/EmployeeRepo.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConfigurationsLibrary.Repositories
+namespace PetShop.EF.Repos
 {
     public class EmployeeRepo : IEntityRepo<Employee>
     {
--- Session-23/Petshop.Web/Program.cs
+++ Session-23/Petshop.Web/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IEntityRepo<Customer>, CustomersRepo>();
+builder.Services.AddScoped<IEntityRepo<Employee>, EmployeeRepo>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
