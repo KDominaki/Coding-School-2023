2ebcb58f52ab2c5b8e47508fff2ad7c3ba71e959
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-02-12 14:57:52 +0200

    S23 - monthly ledger added

---
+++ Session-23/Petshop.Web/Controllers/MonthlyLedgerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Petshop.Web.Controllers
+{
+    public class MonthlyLedgerController : Controller
+    {
+        // GET: MonthlyLedgerController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
