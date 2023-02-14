48fd2ed951b3ed06f15124fe5cd8273dc2a61fa0
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-14 21:50:44 +0200

    Service Task Dto

---
+++ Session-27/CarServiceCenter.Web.Blazor/Shared/ServiceTask/ServiceTaskEditDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenter.Web.Blazor.Shared.ServiceTask
+{
+    public class ServiceTaskEditDto
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public decimal Hours { get; set; }
+    }
+}
