da25b1bea13b8cd4735bf52de310a90032fb575f
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-14 21:28:49 +0200

    Car Dto

---
+++ Session-27/CarServiceCenter.Web.Blazor/Shared/Car/CarListDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenter.Web.Blazor.Shared.Car
+{
+    public class CarListDto
+    {
+        public int Id { get; set; }
+        public string Bramd { get; set; } = string.Empty;
+        public string Model { get; set; }
+        public string CarRegistrationNumber { get; set; }
+    }
+}
