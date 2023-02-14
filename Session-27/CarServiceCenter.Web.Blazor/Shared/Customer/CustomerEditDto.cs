5a6ca5a1ee5cc140403f5bf99b898e1912df3ffa
marinakip <marinakipourou@hotmail.com>
2023-02-14 22:15:07 +0200

    Add CustomerListDto and CustomerEditDto in Shared

---
+++ Session-27/CarServiceCenter.Web.Blazor/Shared/Customer/CustomerEditDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarServiceCenter.Web.Blazor.Shared.Customer {
+    public class CustomerEditDto {
+        public int Id { get; set; }
+        public string Name { get; set; } = null!;
+        public string Surname { get; set; } = null!;
+        public string Phone { get; set; } = null!;
+        public string Tin { get; set; } = null!;
+    }
+}
