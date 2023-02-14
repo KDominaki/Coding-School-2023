6929b5e54c4f9be9c7d62fc40d52d5a5533ae9b1
chrissanthi1995 <chrissanthi1995@yahoo.gr>
2023-02-15 00:27:12 +0200

    car controller

---
+++ Session-27/CarServiceCenter.Web.Blazor/Client/Pages/CarList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CarServiceCenter.Web.Blazor.Client.Pages
+{
+    public class CarListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Session-27/CarServiceCenter.Web.Blazor/Server/Controllers/CarController.cs
@@ -0,0 +1,71 @@
+﻿using CarServiceCenter.EF.Repositories;
+using CarServiceCenter.Model;
+using CarServiceCenter.Web.Blazor.Shared.Car;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace CarServiceCenter.Web.Blazor.Server.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly IEntityRepo<Car> _carRepo;
+        
+        public CarController(IEntityRepo<Car> carRepo)
+        {
+            _carRepo = carRepo;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<CarListDto>> Get()
+        {
+            var result = _carRepo.GetAll();
+            return result.Select(Car => new CarListDto
+            {
+                Id = Car.Id,
+                Model = Car.Model,
+                Bramd = Car.Brand,
+                CarRegistrationNumber = Car.CarRegistrationNumber
+            });
+        }
+
+        [HttpGet("{id}")]
+        public async Task<CarEditDto> GetById(int id)
+        {
+            var result = _carRepo.GetById(id);
+            return new CarEditDto
+            {
+                Id = id,
+                Model = result.Model,
+                Bramd = result.Brand,
+                CarRegistrationNumber = result.CarRegistrationNumber
+            };
+        }
+
+        [HttpPost]
+        public async Task Post(CarEditDto car)
+        {
+            var newCar = new Car(car.Model, car.Bramd, car.CarRegistrationNumber);
+            _carRepo.Add(newCar);
+        }
+
+        [HttpPut]
+        public async Task Put(CarEditDto car)
+        {
+            var carToUpdate = _carRepo.GetById(car.Id);
+            carToUpdate.Model = car.Model;
+            carToUpdate.Brand = car.Bramd;
+            carToUpdate.CarRegistrationNumber = car.CarRegistrationNumber;
+            _carRepo.Update(car.Id, carToUpdate);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task Delete(int id)
+        {
+            _carRepo.Delete(id);
+        }
+
+    }
+}
