5d3a1d2781cf28865f186d85164b74da999166ea
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-02-14 17:02:34 +0200

    S23 - TransactionRepo added

--- Session-27/CarServiceCenter.EF/Repositories/TransactionRepo.cs
+++ Session-27/CarServiceCenter.EF/Repositories/TransactionRepo.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CarServiceCenter.EF.Context;
+using CarServiceCenter.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,55 @@ using System.Threading.Tasks;
 
 namespace CarServiceCenter.EF.Repositories
 {
-    public class TransactionRepo
+    public class TransactionRepo : IEntityRepo<Transaction>
     {
+        public void Add(Transaction entity)
+        {
+            using var context = new CarServiceCenterDbContext();
+            context.Add(entity);
+            context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            using var context = new CarServiceCenterDbContext();
+            var dbTransaction = context.Transactions.Where(transaction => transaction.Id == id).SingleOrDefault();
+            if (dbTransaction is null)
+            {
+                return;
+            }
+            else
+            {
+                context.Remove(dbTransaction);
+                context.SaveChanges();
+            }
+        }
+
+        public IList<Transaction> GetAll()
+        {
+            using var context = new CarServiceCenterDbContext();
+            return context.Transactions.ToList();
+        }
+
+        public Transaction? GetById(int id)
+        {
+            using var context = new CarServiceCenterDbContext();
+            return context.Transactions.Where(transaction => transaction.Id == id).SingleOrDefault();
+        }
+
+        public void Update(int id, Transaction entity)
+        {
+            using var context = new CarServiceCenterDbContext();
+            var dbTransaction = context.Transactions.Where(transaction => transaction.Id == id).SingleOrDefault();
+            if (dbTransaction is null)
+            {
+                return;
+            }
+            else
+            {
+                dbTransaction.TotalPrice = entity.TotalPrice;
+                context.SaveChanges();
+            }
+        }
     }
 }
