a8edc938f5f2e49ad2b781946c017e91880f6bfe
Konstantinos Dimitropoulos <kostasdmtropoulos@gmail.com>
2023-02-16 13:07:57 +0200

    S27 - ML page and controller added

---
+++ Session-27/CarServiceCenter.Web.Blazor/Server/Controllers/MonthlyLedgerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarServiceCenter.Web.Blazor.Server.Controllers
+{
+    public class MonthlyLedgerController : Controller
+    {
+        public IActionResult Get()
+        {
+            return View();
+        }
+    }
+}
